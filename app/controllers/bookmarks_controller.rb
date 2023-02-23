e3aa8494732f9fb012e747c163b36d12afd09c44
Maggie hum <hum.maggie@gmail.com>
2023-02-23 15:42:12 -0500

    bookmarks work

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,8 +3,9 @@ class BookmarksController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    @movie = Movie.find(params[:movie_id])
+    # @movie = Movie.find(params[:movie_id])
     @bookmark.list = @list
+
 
     if @bookmark.save
       redirect_to list_path(@list)
@@ -16,7 +17,7 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
   # def create
 
