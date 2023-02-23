f064a63254b5127a07145ed6f27f2e167da04b2e
Maggie hum <hum.maggie@gmail.com>
2023-02-23 15:31:44 -0500

    bookmark form isnt saving

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,24 @@
+class BookmarksController < ApplicationController
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @movie = Movie.find(params[:movie_id])
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/show', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+  # def create
+
+  # end
+end
