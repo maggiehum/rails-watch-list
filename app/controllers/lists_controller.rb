a536154361922aee056c354ff4e0e614e3083720
Maggie hum <hum.maggie@gmail.com>
2023-02-24 16:09:04 -0500

    cards created

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,7 +17,7 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
 
     if @list.save
-      redirect_to list_path(@list)
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -26,6 +26,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :image)
   end
 end
